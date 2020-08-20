fef1dee53610cc64220d6db8ed592bb63f17b6ce
yuyaoqiang <648989873@qq.com>
2020-08-20 23:50:15 +0800

    优化stack,私有变量

--- weChat/js/stack.js
+++ weChat/js/stack.js
@@ -1,51 +1,63 @@
-// 栈管理
-class Stack {
-  constructor() {
-    this.len = 0; // 栈长度
-    this.stack = {}; // 栈
-  }
+var Stack = (function () {
+    // 属性私有
+    const stack = Symbol('stack')
+    const len = Symbol('len')
 
-  static getStackInstance() {
-    if (!this.instance) {
-      this.instance = new Stack();
+    // 栈管理
+    class Stack {
+        constructor() {
+            this[len] = 0; // 栈长度
+            this[stack] = {}; // 栈
+        }
+
+        static getStackInstance() {
+            if (!this.instance) {
+                this.instance = new Stack();
+            }
+            return this.instance;
+        }
+
+        // 栈顶
+        getStackTop() {
+            const size = this.size();
+            return this[stack][size];
+        }
+
+        // 获取栈中所有数据
+        getStacks() {
+            let result = [];
+            if (this[len] === 0) return [];
+            console.log(this[stack])
+            Object.keys(this[stack]).forEach((s) => result.push(this[stack][s]));
+            return result;
+        }
+
+        size() {
+            return this[len];
+        }
+        // 进栈
+        push(page) {
+            const size = this.size() + 1;
+            this[stack][size] = page;
+            this[len] = size;
+            return this.getStackTop();
+        }
+
+        // 出栈
+        pop() {
+            const stackTop = this.getStackTop();
+            const size = this.size();
+            delete this[stack][size];
+            this[len] = size - 1;
+            return stackTop;
+        }
+
+        //销毁
+        clean() {
+            this[len] = 0;
+            this[stack] = {};
+        }
     }
-    return this.instance;
-  }
-
-  // 栈顶
-  getStackTop() {
-    return this.stack[this.len];
-  }
-
-  // 获取栈中所有数据
-  getStacks() {
-    let result = [];
-    if (this.len === 0) return [];
-    Object.keys(this.stack).forEach((s) => result.push(this.stack[s]));
-    return result;
-  }
-  
-  // 进栈
-  push(page) {
-    this.len += 1;
-    this.stack[this.len] = page;
-    return this.getStackTop();
-  }
-
-  // 出栈
-  pop() {
-    const stackTop = this.getStackTop();
-    delete this.stack[this.len];
-    this.len -= 1;
-    return stackTop;
-  }
-
-  //销毁
-  clean() {
-    this.len = 0;
-    this.stack = {};
-  }
-
-}
-
+    return Stack;
+})()
 export default Stack;
