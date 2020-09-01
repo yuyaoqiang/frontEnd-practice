42719ad89bd03d78503a545c1d4f513640ed47b0
yuyaoqiang <648989873@qq.com>
2020-09-01 23:41:43 +0800

    space 1

--- weChat/pages/home.js
+++ weChat/pages/home.js
@@ -27,49 +27,48 @@ class Home extends Component {
         '这波能反杀',
       ]
     };
-    this.templateHTML=`
-      <div class="page chat-page" id="home">
-						<header>
-						<ul class="info-header">
-						<li></li>
-						<li>{{ title }}</li>
-						<li><span class="icon iconfont icon-icon-"></span></li>
-						</ul>
-						</header>
-						<main class="chat-main-wrap" id="chat-main-wrap">
-						<ul class="next">
-						{{each list  value i}}
-						<li class="row" onclick="wx.go()">
-						<span class="user-icon"> <img src="./icon/icon.jpeg" alt="" /> </span
-						><span class="user-name">{{value}}{{i}}</span>
-						</li>
-						{{/each}}
-						</ul>
-						</main>
-						<footer>
-						<ul class="chat-footer">
-						<li>
-						<span class="iconfont chat icon-cebianlan_liaotianliebiao"></span>
-						<span class="zh-name chat">微信</span>
-						</li>
-						<li>
-						<span class="icon iconfont icon-tongxunlu-copy"></span>
-						<span class="zh-name">通讯录</span>
-						</li>
-						<li>
-						<span class="icon iconfont icon-faxian"></span>
-						<span class="zh-name">发现</span>
-						</li>
-
-						<li>
-						<span class="iconfont icon-wo"></span>
-						<span class="zh-name">我</span>
-						</li>
-						</ul>
-						</footer>
-						</div>
-					`;
-		}
+  this.templateHTML=`
+    <div class="page chat-page" id="home">
+    <header>
+    <ul class="info-header">
+    <li></li>
+    <li>{{ title }}</li>
+    <li><span class="icon iconfont icon-icon-"></span></li>
+    </ul>
+    </header>
+    <main class="chat-main-wrap" id="chat-main-wrap">
+    <ul class="next">
+    {{each list  value i}}
+    <li class="row" onclick="wx.go()">
+    <span class="user-icon"> <img src="./icon/icon.jpeg" alt="" /> </span
+    ><span class="user-name">{{value}}{{i}}</span>
+    </li>
+    {{/each}}
+    </ul>
+    </main>
+    <footer>
+    <ul class="chat-footer">
+    <li>
+    <span class="iconfont chat icon-cebianlan_liaotianliebiao"></span>
+    <span class="zh-name chat">微信</span>
+    </li>
+    <li>
+    <span class="icon iconfont icon-tongxunlu-copy"></span>
+    <span class="zh-name">通讯录</span>
+    </li>
+    <li>
+    <span class="icon iconfont icon-faxian"></span>
+    <span class="zh-name">发现</span>
+    </li>
+    <li>
+    <span class="iconfont icon-wo"></span>
+    <span class="zh-name">我</span>
+    </li>
+    </ul>
+    </footer>
+    </div>
+    `;
+  }
 
 go(){
 history.push({state:'chat'},'聊天页','chat')
